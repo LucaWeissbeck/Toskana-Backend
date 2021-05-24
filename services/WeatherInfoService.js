8b3f1782b954264f72b36c9b963693409843e888
LucaWeissbeck <lucaweissbeck@yahoo.de>
2021-05-24 21:40:32 +0200

    Initial Commit + authorisation method for Netatmo Weather

---
+++ services/WeatherInfoService.js
@@ -0,0 +1,8 @@
+const axios = require("axios");
+
+const clientSecret = process.env.CLIENT_SECRET;
+const clientID = process.env.CLIENT_ID
+
+const getWeatherInfoCurrent = async(req, res) => {
+    
+};
