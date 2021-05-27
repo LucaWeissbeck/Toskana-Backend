07e7b40ee26ebbb01ee9429b1639cce9573e22ea
LucaWeissbeck <lucaweissbeck@yahoo.de>
2021-05-27 23:40:24 +0200

    Added routes

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors')
 
 var authRouter = require('./routes/netatmoauthorisation');
 var weatherRouter = require('./routes/weather');
@@ -18,6 +19,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
+
 
 app.use('/authorise', authRouter);
 app.use('/weather', weatherRouter);
