b85a9d81a0574243799489776a2e4c1306859fc2
Luca Weissbeck <Luca.Weissbeck@ibm.com>
2021-06-12 13:48:05 +0200

    Added Async MySQL call

--- routes/phValue.js
+++ routes/phValue.js
@@ -4,7 +4,14 @@ const phMeasureService = require("../services/phMeasureService");
 
 /* Returns complete present Data for outdoor & indoor module */
 router.get('/all', async(req, res) => {
-    phMeasureService.getAllValues()
+    try{
+        const data = await(phMeasureService.getAllValues()); 
+        res.status(200).send(data);
+    }
+    catch(err){
+        console.error(err);
+        res.status(400).send("Request failed");
+    }
 });
 
 module.exports = router;
--- services/phMeasureService.js
+++ services/phMeasureService.js
@@ -1,13 +1,15 @@
 const db = require("./db");
 
 const getAllValues = () => {
-    const test = db.query("SELECT * FROM PH", (err, rows) => {
-        if(err) throw err;
+    return new Promise((resolve, reject) => {
+        db.query("SELECT * FROM PH", (err, rows) => {
+            if(err){
+                return reject(new err)
+            }
+            resolve(rows);
+        })
 
-        console.log("Data received from DB:");
-        console.log(rows);
     })
-    console.log(test)
 }
 
 module.exports.getAllValues = getAllValues;
